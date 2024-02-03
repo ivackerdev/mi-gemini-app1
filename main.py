cf9b107e7d1bb9b80a77c6a9d79bbd7bb9f19751
ivackerdev <ivackerdev@gmail.com>
2024-02-03 17:30:02 +0000

    fc

---
+++ main.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+import google.generativeai as genai
+from flask import Flask, jsonify, request, send_file, send_from_directory
+
+# 🔥 FILL THIS OUT FIRST! 🔥
+# 🔥 GET YOUR GEMINI API KEY AT 🔥
+# 🔥 https://makersuite.google.com/app/apikey 🔥
+API_KEY = 'AIzaSyAoYD69JLjoYXyEW5JkkRfgLRvGM3Lx5Ro'
+
+genai.configure(api_key=API_KEY)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return send_file('web/index.html')
+
+
+@app.route("/api/generate", methods=["POST"])
+def generate_api():
+    if request.method == "POST":
+        if API_KEY == 'TODO':
+            return jsonify({ "error": '''
+                To get started, get an API key at
+                https://makersuite.google.com/app/apikey and enter it in
+                main.py
+                '''.replace('\n', '') })
+        try:
+            req_body = request.get_json()
+            content = req_body.get("contents")
+            model = genai.GenerativeModel(model_name=req_body.get("model"))
+            response = model.generate_content(content, stream=True)
+            def stream():
+                for chunk in response:
+                    yield 'data: %s\n\n' % json.dumps({ "text": chunk.text })
+
+            return stream(), {'Content-Type': 'text/event-stream'}
+
+        except Exception as e:
+            return jsonify({ "error": str(e) })
+
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('web', path)
+
+
+if __name__ == "__main__":
+    app.run(port=int(os.environ.get('PORT', 80)))
